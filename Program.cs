11485975d683618f87121f345f412f6740a2662a
David Jasso <95830446+Davdick@users.noreply.github.com>
2025-01-26 09:24:36 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+        bool isPrime(int n)
+        {
+            if(n <= 1)
+                return false;
+            for(int i = 2; i<= Math.Sqrt(n); i++)
+            {
+                if(n % i == 0)
+                    return false;
+            }
+            return true;
+        }
+
+
+Console.WriteLine(isPrime(10));
